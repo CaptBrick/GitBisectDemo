52979fd45ebe77a5fbdf9b37631b72940af15d03
Rytis Ilciukas <Rytis.Ilciukas@xavo.com>
2015-08-10 07:23:55 +0200

    add initial program

--- PhoneNumberValidator/Program.cs
+++ PhoneNumberValidator/Program.cs
@@ -1,9 +1,33 @@
-﻿namespace PhoneNumberValidator
+﻿using System;
+
+namespace PhoneNumberValidator
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
+            var program = new Program();
+            program.Run();
+        }
+
+        public void Run()
+        {
+            while (true)
+            {
+                var validator = new Validator();
+                Console.WriteLine("Enter phone number:");
+                var input = Console.ReadLine();
+                Console.WriteLine();
+                Console.Write("{0} is ", input);
+                if (validator.IsNumberValid(input))
+                {
+                    Console.WriteLine("valid phone number");
+                }
+                else
+                {
+                    Console.WriteLine("invalid phone number");
+                }
+            }
         }
     }
 }
