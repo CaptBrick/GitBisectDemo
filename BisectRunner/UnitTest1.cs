c14219d452d8dd535aa104702c3f9f589bbc282c
Rytis Ilciukas <Rytis.Ilciukas@xavo.com>
2015-08-10 10:00:26 +0200

    add bisect runner

---
+++ BisectRunner/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PhoneNumberValidator;
+
+namespace BisectUnitTest
+{
+
+	//this must be outside versino control
+    [TestClass]
+    public class UnitTest1
+    {
+        private Validator _validator;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _validator = new Validator();
+        }
+
+        [TestMethod]
+        public void TestPhoneNumberWithPlusInMiddle()
+        {
+            var isValid = _validator.IsNumberValid("123+123");
+            Assert.IsFalse(isValid);
+        }
+    }
+}
--- BisectRunner/run_unit_tests.bat
+++ BisectRunner/run_unit_tests.bat
@@ -1,10 +1,11 @@
 REM git bisect run cmd /K ..\BisectRunner\run_unit_tests.bat
-
 SET MSBUILD="C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe"
 SET MSTEST="C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\MSTest.exe"
 
-SET SOLUTION=%~dp0%..\PhoneNumberValidator.sln
-SET TEST_CONTAINER=%~dp0%..\PhoneNumberValidator.Test\bin\Debug\PhoneNumberValidator.Test.dll
+SET SOLUTION=%~dp0%BisectUnitTest.sln
+SET TEST_CONTAINER=%~dp0%bin\Debug\BisectUnitTest.dll
 
 %MSBUILD% %SOLUTION%
 %MSTEST% /testcontainer:%TEST_CONTAINER%
+
+exit
