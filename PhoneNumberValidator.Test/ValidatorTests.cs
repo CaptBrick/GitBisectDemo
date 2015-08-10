ab0a3839269c9a05c9319845a2f110ae2ec50cef
Rytis Ilciukas <Rytis.Ilciukas@xavo.com>
2015-08-10 07:13:41 +0200

    add unit tests

---
+++ PhoneNumberValidator.Test/ValidatorTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PhoneNumberValidator.Test
+{
+    [TestClass()]
+    public class ValidatorTests
+    {
+        private Validator _validator;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _validator = new Validator();
+        }
+
+        [TestMethod]
+        public void TestNumbersOnly()
+        {
+            var isValid = _validator.IsNumberValid("123456");
+            Assert.IsTrue(isValid);
+        }
+    }
+}
